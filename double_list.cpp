485bf61e1a64061e57c522976e7df5436e4c6f17
Nikita <nikitakortrlr020@gmail.com>
2025-10-30 01:52:24 +0700

    дописаны файлы

--- double_list.cpp
+++ double_list.cpp
@@ -126,3 +126,162 @@ void DelHead(DList &list)
    delete tmp;
    cout << "Первый элемент удалён" << endl;
 }
+
+// Удаление последнего элемента
+void DelTail(DList &list)
+{
+   if (!list.tail)
+   {
+      cout << "Список пуст" << endl;
+      return;
+   }
+   Node *tmp = list.head;
+   list.tail = list.tail->prev;
+
+   if (list.tail)
+      list.tail->next = nullptr;
+   else
+      list.head = nullptr;
+
+   delete tmp;
+   cout << "Последний элемент удалён";
+}
+
+// Удалить определенный элемент
+void DelVal(DList &list, const string &val)
+{
+   if (!list.head)
+   {
+      cout << "Список пуст." << endl;
+      return;
+   }
+   Node *curr = list.head;
+   while (curr && curr->data != val)
+   {
+      curr = curr->next;
+   }
+
+   if (!curr)
+   {
+      cout << "Элемент " << val << " не найден." << endl;
+      return;
+   }
+
+   if (curr->prev)
+      curr->prev->next = curr->next;
+   else
+      list.head = curr->next;
+
+   if (curr->next)
+      curr->next->prev = curr->prev;
+   else
+      list.tail = curr->prev;
+
+   delete curr;
+   cout << "Элемент \"" << val << "\" удален." << endl;
+}
+
+// Поиск по значению
+Node *findVal(const DList &list, const string &val)
+{
+   for (Node *curr = list.head; curr; curr = curr->next)
+   {
+      if (curr->data == val)
+      {
+         return curr;
+      }
+   }
+   return nullptr;
+}
+
+// Вывод списка с начала
+void PrintForward(const DList &list)
+{
+   if (!list.head)
+   {
+      cout << "Список пуст" << endl;
+      return;
+   }
+
+   cout << "Список вперед: ";
+   for (Node *curr = list.head; curr; curr = curr->next)
+      cout << curr->data << " ";
+   cout << endl;
+}
+
+// Вывод с конца
+void PrintBackward(const DList &list)
+{
+   if (!list.tail)
+   {
+      cout << "Список пуст" << endl;
+      return;
+   }
+
+   cout << "Список назад: ";
+   for (Node *curr = list.tail; curr; curr = curr->prev)
+   {
+      cout << curr->data << " ";
+   }
+   cout << endl;
+}
+
+// Отчистка списка
+void ClearList(DList &list)
+{
+   while (list.head)
+   {
+      Node *tmp = list.head;
+      list.head = list.head->next;
+      delete tmp;
+   }
+   list.tail = nullptr;
+}
+
+// Удаление после элемента
+void DelAfterVal(DList &list, const string &val)
+{
+   Node *targetNode = findVal(list, val);
+   if (!targetNode || !targetNode->next)
+   {
+      cout << "Элемент '" << val << "' не найден или он последний." << endl;
+      return;
+   }
+   Node *nodeToDelete = targetNode->next;
+   // Если удаляемый узел - хвост, обновляем хвост
+   if (nodeToDelete == list.tail)
+   {
+      DelTail(list);
+   }
+   else
+   {
+      targetNode->next = nodeToDelete->next;
+      nodeToDelete->next->prev = targetNode;
+      delete nodeToDelete;
+   }
+   cout << "Элемент после '" << val << "' удален." << endl;
+}
+
+// Удаление до элемента
+void DelBeforeVal(DList &list, const string &val)
+{
+   Node *targetNode = findVal(list, val);
+   if (!targetNode || !targetNode->prev)
+   {
+      cout << "Элемент '" << val << "' не найден или он первый." << endl;
+      return;
+   }
+   Node *nodeToDelete = targetNode->prev;
+   // Если удаляемый узел - голова, обновляем голову
+   if (nodeToDelete == list.head)
+   {
+      DelHead(list);
+   }
+   else
+   {
+      targetNode->prev = nodeToDelete->prev;
+      nodeToDelete->prev->next = targetNode;
+      delete nodeToDelete;
+   }
+   cout << "Элемент перед '" << val << "' удален." << endl;
+}
--- double_list.h
+++ double_list.h
@@ -18,5 +18,19 @@ struct DList
    Node* tail; // Указатель на предыдущий элемент
 };
 
+void CreateList(DList &list);
+void AddHead(DList &list);
+void AddToTail(DList &list, string val);
+void AddBeforeVal(DList &list);
+void AddAfterVal(DList &list);
+void DelHead(DList &list);
+void DelTail(DList &list);
+void DelVal(DList &list, const string &val);
+Node *findVal(const DList &list, const string &val);
+void PrintForward(const DList &list);
+void PrintBackward(const DList &list);
+void ClearList(DList &list);
+void DelAfterVal(DList &list, const string &val);
+void DelBeforeVal(DList &list, const string &val);
 
 #endif
